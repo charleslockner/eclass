efc91b5542970350e386b44c50b50a0fe6d428d0
Keith Abdulla <email@ekeitho.com>
2014-11-25 21:59:51 -0800

    create a viewer test

---
+++ implementation/src/test/ViewerTest.java
@@ -0,0 +1,90 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ViewerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentClasses() throws Exception {
+
+    }
+
+    @Test
+    public void testSetAble2Draw() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCurrentlyInClass() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGrade() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLastName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAble2Draw() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentlyInClass() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRole() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetClassCount() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateViewer() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateViewer1() throws Exception {
+
+    }
+
+    @Test
+    public void testUnmarshallViewerDataFromServer() throws Exception {
+
+    }
+}
--- implementation/src/view/roster/ProfClassView.java
+++
@@ -1,5 +0,0 @@
-package view.roster;
-
-public abstract class ProfClassView {
-
-}
