b3dc1ca5e019619d69741d51d706d5a7cfa2ec82
Keith Abdulla <email@ekeitho.com>
2014-11-25 23:30:05 -0800

    layers test

---
+++ implementation/src/test/LayerViewTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LayerViewTest {
+
+    @Test
+    public void testCreateImageIcon() throws Exception {
+
+    }
+
+    @Test
+    public void testAddLayer() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveLayer() throws Exception {
+
+    }
+}
