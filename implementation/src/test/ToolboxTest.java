cc3eb78399a6fe74d0fa08403c0694392c1918de
hobbes1021 <hobbes1021@gmail.com>
2014-12-09 16:14:52 -0800

    added ToolboxTest.java

---
+++ implementation/src/test/ToolboxTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+import util.tools.Text;
+import util.tools.Canvas;
+
+/**
+ * Test case for the presentation model class.
+ * @author Eric Yun (eryun@calpoly.edu)
+ */
+public class ToolboxTest {
+
+    public void testTextGetLocation() throws Exception {
+
+    }
+
+    public void testTextSetLocation() throws Exception {
+
+    }
+
+    public void testTextGetBlock() throws Exception {
+
+    }
+
+    public void testTextSetBlock() throws Exception {
+
+    }
+
+    public void testCanvasAdd() throws Exception {
+
+    }
+
+    public void testCanvasGet() throws Exception {
+
+    }
+}
